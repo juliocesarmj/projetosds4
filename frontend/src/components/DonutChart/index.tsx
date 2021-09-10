ea8b6472cacef0ebeaf5867d292ed49bbd8bcb10
Julio Cesar <juliocesardemoraes2015@gmail.com>
2021-09-10 14:51:51 -0300

    First request

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,5 +1,24 @@
+import axios from 'axios';
 import Chart from 'react-apexcharts';
+import { SaleSum } from 'Types/sale';
+import { BASE_URL } from 'utils/requests';
+
+type ChartData = {
+  labels: string[];
+  series: number[];
+}
 const DonutChart = () => {
+
+  let chartdata: ChartData = { labels: [], series: [] };
+
+  axios.get(`${BASE_URL}/sales/amount-by-seller`)
+    .then(r => {
+      const data = r.data as SaleSum[];
+      const myLabels = data.map(x => x.sellerName);
+      const mySeries = data.map(x => x.sum);
+      chartdata = { labels: myLabels, series: mySeries };
+      console.log(chartdata);
+    })
   const mockData = {
     series: [477138, 499928, 444867, 220426, 473088],
     labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
