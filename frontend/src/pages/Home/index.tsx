1c046811d1c6e1bfd38e7d6f1eb68c4c65bb9d4c
Julio Cesar <juliocesardemoraes2015@gmail.com>
2021-09-11 21:09:43 -0300

    padding index

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -7,7 +7,7 @@ const Home = () => {
     <>
       <Navbar />
       <div className="container">
-        <div className="jumbotron mt-5">
+        <div className="jumbotron">
           <h1 className="display-4">DSVendas</h1>
           <p className="lead">Analise o desempenho das suas vendas por diferentes perspectivas</p>
           <hr />
